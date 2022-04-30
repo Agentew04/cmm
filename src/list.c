05038585876256cf0558f067189180b38a39e72c
Rodrigo Appelt <digoappelt@gmail.com>
2022-04-30 17:20:40 -0300

    fixed params

--- src/list.c
+++ src/list.c
@@ -1,22 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "list.h"
+#include <stdbool.h>
 
-void printList(LIST *list){
-    struct Node *current = list->head;
+// #region Creates
 
-    while (current != NULL){
-        printf("%d\n", current->value);
-        current = current->next;
-    }   
+static struct Node *createNode(int value, struct Node *next)
+{
+    struct Node *node = malloc(sizeof *node);
+    node->value = value;
+    node->next = next;
+    return node;
 }
 
-void appendToList(LIST *list, int value){
-    struct Node* current = list->head;
-    struct Node* newNode = createNode(value, NULL);
+LIST *createList(int *values, int size)
+{
+    // check if new list should be empty
+    LIST *list = malloc(sizeof *list);
+    list->size = size;
+    if (size == 0 || values == NULL)
+    {
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    else
+    {
+        list->head = createNode(values[0], NULL);
+        list->tail = list->head;
+    }
+
+    // if empty the loop will not activate
+    for (int i = 1; i < size; i++)
+    {
+        appendToList(list, values[i]);
+        list->tail = list->tail->next;
+    }
+    return list;
+}
+
+// #endregion
+
+// #region interacts
+
+void appendToList(LIST *list, int value)
+{
+    struct Node *current = list->head;
+    struct Node *newNode = createNode(value, NULL);
 
     // if empty list
-    if(current == NULL){
+    if (current == NULL)
+    {
         list->head = newNode;
         list->tail = newNode;
         list->size++;
@@ -24,22 +57,90 @@ void appendToList(LIST *list, int value){
     }
 
     // get to last node
-    while(current->next!=NULL) current = current->next;
+    while (current->next != NULL)
+        current = current->next;
 
     // here current is the last
     current->next = newNode;
 }
 
-struct Node* createNode(int value, struct Node* next){
-    struct Node *node = malloc(sizeof *node);
-    node->value = value;
-    node->next = next;
-    return node;
+void removeFirstValueFromList(LIST *list, int value)
+{
+    struct Node *current = list->head;
+    struct Node *previous = NULL;
+
+    while (current != NULL)
+    {
+        if (current->value == value)
+        {
+            // if the value is the first element
+            if (previous == NULL) list->head = current->next;
+            else previous->next = current->next;
+            free(current);
+            list->size--;
+            return;
+        }
+        else
+        {
+            // go to next node
+            previous = current;
+            current = current->next;
+        }
+    }
 }
 
-void freeList(LIST *list){
-    struct Node* temp;
-    while(list->head != NULL){
+// #endregion
+
+// #region search
+
+bool existsInList(LIST* list, int value)
+{
+    struct Node *current = list->head;
+    while (current != NULL)
+    {
+        if (current->value == value)
+            return true;
+        current = current->next;
+    }
+    return false;
+}
+
+int getFirstIndex(LIST *list, int value)
+{
+    struct Node *current = list->head;
+    int index = 0;
+
+    while (current != NULL)
+    {
+        if (current->value == value) return index;
+        else
+        {
+            index += 1;
+            current = current->next;
+        }
+    }
+    return -1;
+}
+
+// #endregion
+
+
+void printList(LIST *list)
+{
+    struct Node *current = list->head;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
+
+void freeList(LIST *list)
+{
+    struct Node *temp;
+    while (list->head != NULL)
+    {
         temp = list->head;
         list->head = list->head->next;
         free(temp);
@@ -47,69 +148,13 @@ void freeList(LIST *list){
     free(list);
 }
 
-int getFirstIndex(struct Node* head, int value){
-    struct Node* current = head;
-    int index = 0;
 
-    while (current != NULL)
-    {
-        if(current->value == value){
-            return index;
-        }else{
-            index+=1;
-            current = current->next;
-        }
-    }
-    return -1;
-}
+int main()
+{
+    int values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-void removeValueFromList(struct Node* head, int value){
-    struct Node* current = head;
-    struct Node* previous = NULL;
-
-    while (current != NULL)
-    {
-        if(current->value == value){
-            if(previous == NULL){
-                // if the value is the first element
-                head = current->next;
-            }else{
-                previous->next = current->next;
-            }
-            free(current);
-            return;
-        }else{
-            previous = current;
-            current = current->next;
-        }
-    }
-}
-
-LIST* createList(int* values, int size){
-    // check if new list should be empty
-    LIST* list = malloc(sizeof *list);
-    list->size = size;
-    if(size == 0 || values == NULL){
-        list->head = NULL;
-        list->tail = NULL;
-    }else{
-        list->head = createNode(values[0], NULL);
-        list->tail = list->head;
-    }
-
-    // if empty the loop will not activate
-    for(int i=1; i<size; i++){
-        appendToList(list, values[i]);
-        list->tail = list->tail->next;
-    }
-    return list;
-}
-
-int main(){
-    int values[] = {1,2,3,4,5,6,7,8,9,10};
-
-    //LIST* list = createList(&values[0], (sizeof values)/sizeof(int));
-    LIST* list = createList(NULL, 0);
+    // LIST* list = createList(&values[0], (sizeof values)/sizeof(int));
+    LIST *list = createList(NULL, 0);
 
     appendToList(list, 11);
     printList(list);
--- src/list.h
+++ src/list.h
@@ -33,7 +33,7 @@ void appendToList(LIST* list, int value);
  * 
  * @return The pointer to the Node
  */
-struct Node* createNode(int value, struct Node* next);
+static struct Node* createNode(int value, struct Node* next);
 
 /**
  * @brief Frees the resources related to the nodes subsequent
@@ -45,23 +45,27 @@ void freeList(LIST* head);
 /**
  * @brief Get the index of the first ocurrence of a value
  * 
- * @param head The first node in the list
+ * @param list The first node in the list
  * @param value The value to be searched
- * @return The index of the first ocurrence of the value
+ * @return The index of the first ocurrence of the value.
+ * If the value is not found, returns -1
  */
-int getFirstIndex(struct Node* head, int value);
+int getFirstIndex(LIST *list, int value);
 
 /**
  * @brief Removes the first ocurrence of a value from a list
  * 
- * @param head The first node from the list
+ * @param list The first node from the list
  * @param value The value to be removed
  */
-void removeValueFromList(struct Node* head, int value);
+void removeFirstValueFromList(LIST *list, int value);
 
 /**
- * @brief Create a List object
+ * @brief Creates a list based on a array. If values or
+ * size are respectively NULL or 0, the list will be empty
  * 
- * @return An list filled with the values
+ * @param values a pointer to many values
+ * @param size how many values are in the array/pointer
+ * @return A pointer to the list object
  */
 LIST* createList(int* values, int size);
