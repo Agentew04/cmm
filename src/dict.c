fbc48b03e5b26751b07135674dba31cf56926e40
Rodrigo Appelt <digoappelt@gmail.com>
2022-05-08 01:03:22 -0300

    add more dict functions

--- src/dict.c
+++ src/dict.c
@@ -2,45 +2,127 @@
 #include <stdlib.h>
 #include "dict.h"
 
-static struct Node* createNode(int key, int value, struct Node* next){
+#define ARRAY_SIZE(x) ( sizeof(x) / sizeof((x)[0]) )
+
+static struct Node* createNode(char* key, int value, struct Node *next){
     struct Node* node = (struct Node*) malloc(sizeof(struct Node));
-    node->value = value;
     node->next = next;
-    node->key = key;
+    int hash = crc32b(key);
+    struct KeyValuePair* pair = (struct KeyValuePair*) malloc(sizeof(struct KeyValuePair));
+    pair->key = key;
+    pair->value = value;
+    node->hash = hash;
+    node->pair = pair;
     return node;
 }
 
-void appendToDict(DICT* list, int key, int value){
+void addToDict(DICT *dict, char *key, int value){
     struct Node* node = createNode(key, value, NULL);
-    if(list->head == NULL){
-        list->head = node;
-        list->tail = node;
+    if(dict->head == NULL){
+        dict->head = node;
+        dict->tail = node;
     }else{
-        list->tail->next = node;
-        list->tail = node;
+        dict->tail->next = node;
+        dict->tail = node;
     }
-    list->size++;
+    dict->size++;
 }
 
-DICT* createDict(int* keys, int* values){
-    int keysSize = sizeof(*keys) / sizeof(int);
-    int valuesSize = sizeof(*values) / sizeof(int);
-
-    if(keysSize != valuesSize){
-        //printf("Error: The size of the arrays are not equal");
-        return NULL;
+void removeFromDict(DICT *dict, int key){
+    struct Node* node = dict->head;
+    unsigned int hash = crc32b(key);
+    struct Node* prev = NULL;
+    while(node != NULL){
+        if(node->hash==hash){
+            if(prev == NULL){
+                // if its the first node
+                dict->head = node->next;
+            }else{
+                prev->next = node->next;
+            }   
+            free(node->pair);
+            free(node);
+            dict->size--;
+            return;
+        }
+        prev = node;
+        node = node->next;
     }
+}
 
+int getValueFromDict(DICT *dict, int key){
+
+    
+    unsigned int hash = crc32b(key);
+    struct Node* node = dict->head;
+    while(node != NULL){
+        if(node->hash == hash){
+            return node->pair->value;
+        }
+        node = node->next;
+    }
+    return -1;
+}
+
+void editValueInDict(DICT *dict, int key, int value){
+    unsigned int hash = crc32b(key);
+    struct Node* node = dict->head;
+    while(node != NULL){
+        if(node->hash == hash){
+            node->pair->value = value;
+            return;
+        }
+        node = node->next;
+    }
+}
+
+int keyExistsInDict(DICT *dict, int key){
+    int count = 0;
+    unsigned int hash = crc32b(key);
+    struct Node* node = dict->head;
+    while(node != NULL){
+        if(node->hash == hash){
+            count++;
+        }
+        node = node->next;
+    }
+    return count;
+}
+
+
+DICT* createEmptyDict(){
     DICT* dict = (DICT*) malloc(sizeof(DICT));
     dict->size = 0;
     dict->head = NULL;
     dict->tail = NULL;
+    return dict;
+}
+DICT* createDict(char* key[], int value[]){
+    int keySize = ARRAY_SIZE(key);
+    int valueSize = ARRAY_SIZE(value);
+    DICT* dict = createEmptyDict();
 
-    for(int i = 0; i < keysSize; i++){
-        appendToDict(dict, keys[i], values[i]);
+    if(keySize != valueSize){
+        printf("Error: key and value arrays are not the same size");
+        return NULL;
     }
 
+    for(int i = 0; i < keySize; i++){
+        addToDict(dict, key[i], value[i]);
+    }
     return dict;
+}
+
+void freeDict(DICT *dict){
+    struct Node* node = dict->head;
+    struct Node* next;
+    while(node != NULL){
+        next = node->next;
+        free(node->pair);
+        free(node);
+        node = next;
+    }
+    free(dict);
 }
 
 static unsigned int crc32b(unsigned char *message) {
--- src/dict.h
+++ src/dict.h
@@ -1,8 +1,14 @@
-typedef struct Node {
-    struct Node *next;
-    int key;
+struct KeyValuePair
+{
+    char *key;
     int value;
-} NODE;
+}; 
+
+struct Node {
+    struct Node *next;
+    unsigned int hash;
+    struct KeyValuePair *pair;
+};
 
 typedef struct Dict{
     struct Node *head;
@@ -19,8 +25,7 @@ typedef struct Dict{
  * 
  * @return The pointer to the Node
  */
-static struct Node* createNode(int key, int value, struct Node* next);
-
+static struct Node* createNode(char* key, int value, struct Node *next);
 /**
  * @brief Appends a value to the end of a dictonary
  * 
@@ -28,19 +33,62 @@ static struct Node* createNode(int key, int value, struct Node* next);
  * @param key The key of the node
  * @param value The value to be appended
  */
-void appendToDict(DICT* list, int key, int value);
+void addToDict(DICT *dict, char *key, int value);
+/**
+ * @brief Removes an entry from the dictionary
+ * 
+ * @param list the dict object
+ * @param key The key for the entry to be removed
+ */
+void removeFromDict(DICT *dict, int key);
+/**
+ * @brief Get the value associated with a key. If there are two
+ * entries with the same key, the first one is returned. Check
+ * with `keyExistsInDict()` if there are more than one entry with the same key.
+ * 
+ * @param dict The dict object
+ * @param key The key for the value to be returned
+ * @return the value associated with the key. Returns -1 if key is not found
+ */
+int getValueFromDict(DICT *dict, int key);
+/**
+ * @brief Changes the value associated with a key
+ * 
+ * @param dict The dict object
+ * @param key The key for the value to be changed
+ * @param value The new value to be associated with the key
+ */
+void editValueInDict(DICT *dict, int key, int value);
+/**
+ * @brief Get how many entries are in the dictionary with the same key
+ * 
+ * @param dict The dict object
+ * @param key The key to be searched
+ * @return The amount of entries with the same key
+ */
+int keyExistsInDict(DICT *dict, int key);
 
 /**
- * @brief Creates a new dictonary base on two arrays, 
- * one with the keys and the other with the values. 
- * Note that the arrays must have the same size
+ * @brief Create an empty Dictionary
  * 
- * @param keys A ptr to an array
- * @param values A ptr to an array
- * @return A pointer to the dict object. 
- * NULL if the size of the arrays are not equal
+ * @return A pointer to the dict object
  */
-DICT* createDict(int* keys, int* values);
+DICT* createEmptyDict();
+/**
+ * @brief Creates a dictionary based on two arrays. 
+ * Must be of same size
+ * 
+ * @param keys The keys of the dictionary
+ * @param values The values of the dictionary
+ * @return A ptr to the dict object
+ */
+DICT* createDict(char* keys[], int values[]);
+/**
+ * @brief Frees an entire dictionary from memory
+ * 
+ * @param dict The dict object to be freed
+ */
+void freeDict(DICT *dict);
 
 /**
  * @brief Hashes a string using crc32
